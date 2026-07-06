57c9ae820e9fea9555ec3c5c3b2922e82b3c72c4
abhijeetmahamune <112451250+abhijeetmahamune@users.noreply.github.com>
2026-07-06 16:22:09 +0530

    fix: remove invalid eslint key from next.config.ts in Next.js 16

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  eslint: {
-    // Allow production builds to complete even if there are ESLint errors.
-    ignoreDuringBuilds: true,
-  },
+  /* config options here */
 };
 
 export default nextConfig;
